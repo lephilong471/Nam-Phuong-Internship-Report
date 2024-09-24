e1fcc2d413bc47fcc08fe15414424460892f1304
unknown <lephilong471@gmail.com>
2024-09-24 12:25:16 +0700

    Week 9

--- Exercise/TypeScript + ReactJS/vite-project/src/App.tsx
+++ Exercise/TypeScript + ReactJS/vite-project/src/App.tsx
@@ -1,7 +1,39 @@
 import { ChangeEvent, useState } from 'react'
 import { returnPaginationRange, getTableData } from './global'
 import './App.css'
-import { useQuery, useMutation} from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { styled } from '@mui/material/styles';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
 
 interface TableDataProps {
   userId: number,
@@ -34,10 +66,10 @@ function App() {
   const fetchData = async (pageNumber: number, rowLimit: number): Promise<TableDataProps[]> => {
     const data: TableDataProps[] = await getDataFromAPI()
     setAllRows(data.length)
-    return getTableData(data, pageNumber, rowLimit )
+    return getTableData(data, pageNumber, rowLimit)
   }
 
-  
+
   const { data } = useQuery<TableDataProps[]>(
     {
       queryKey: ['getTableData', currentPage],
@@ -56,7 +88,7 @@ function App() {
         'Content-Type': 'application/json',
       },
     });
-  
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -83,37 +115,37 @@ function App() {
 
   const [paginate, setPaginate] = useState<Array<number | string>>(returnPaginationRange(Math.ceil(20 / limit), currentPage, 1))
 
-    const handlePageChange = (pageChange: number) => {
-      setCurrentPage(pageChange)
-      // setPageData(getTableData(data, pageChange, limit))
-      setPaginate(returnPaginationRange(Math.ceil(20 / limit), pageChange, 1))
-    }
-  
+  const handlePageChange = (pageChange: number) => {
+    setCurrentPage(pageChange)
+    // setPageData(getTableData(data, pageChange, limit))
+    setPaginate(returnPaginationRange(Math.ceil(20 / limit), pageChange, 1))
+  }
 
 
 
-    // setPageData(getTableData(data, page, limit))
+
+  // setPageData(getTableData(data, page, limit))
   // setPaginate(returnPaginationRange(Math.ceil(20 / limit), currentPage, 1))
-    // setTotalPage(Math.ceil(data.length / limit)) 
+  // setTotalPage(Math.ceil(data.length / limit)) 
 
 
   // setAllData(data)
   const [createInfo, setcreateInfo] = useState({
     title: '',
-    body: '' 
+    body: ''
   })
 
   const changeFormCreate = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      const {value, name} = event.target
-      setcreateInfo(prev => ({
-          ...prev, [name]: value
-      }))
+    const { value, name } = event.target
+    setcreateInfo(prev => ({
+      ...prev, [name]: value
+    }))
   }
 
   const createPost = async () => {
     const response = await fetch(`http://localhost:3000/posts`, {
       method: 'POST',
-      body:JSON.stringify({
+      body: JSON.stringify({
         "id": (allRows + 1).toString(),
         "userId": 1,
         "title": createInfo.title,
@@ -123,7 +155,7 @@ function App() {
         'Content-Type': 'application/json',
       },
     });
-  
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -152,7 +184,7 @@ function App() {
   })
 
   const handleUpdate = (postInfo: TableDataProps) => {
-    const {id, title, body} = postInfo
+    const { id, title, body } = postInfo
     setEdit({
       id: id,
       edit_title: title,
@@ -161,9 +193,9 @@ function App() {
     setModal(true)
   }
   const changeFormEdit = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {value, name} = event.target
+    const { value, name } = event.target
     setEdit(prev => ({
-        ...prev, [name]: value
+      ...prev, [name]: value
     }))
   }
 
@@ -182,7 +214,7 @@ function App() {
   const updatePost = async (postId: number) => {
     const response = await fetch(`http://localhost:3000/posts/${postId}`, {
       method: 'PATCH',
-      body:JSON.stringify({
+      body: JSON.stringify({
         "title": edit.edit_title,
         "body": edit.edit_body
       }),
@@ -190,7 +222,7 @@ function App() {
         'Content-Type': 'application/json',
       },
     });
-  
+
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -198,62 +230,102 @@ function App() {
 
   return (
     <>
-    {modal && (
-    <div id="myModal" className="modal">
-      <div className="modal-content">
-          <span className="close-btn" onClick={() => setModal(false)}>&times;</span>
-          <h2>Update Information</h2>
-          <form onSubmit={() => updatePostMutation.mutate(edit.id)}>
-            Title: <input type="text" name="edit_title" value={edit.edit_title} onChange={changeFormEdit} required/> <br/>
-            Body: <textarea name="edit_body" value={edit.edit_body} onChange={changeFormEdit} required/> <br />
-          <button type="submit">Submit</button>
-          </form>
-      </div>
-    </div>
-    )}
+      {modal && (
+        <div id="myModal" className="modal">
+          <div className="modal-content">
+            <span className="close-btn" onClick={() => setModal(false)}>&times;</span>
+            <h2>Update Information</h2>
+            <Box sx={{
+              textAlign:"left",
+              ".MuiTextField-root":{
+                margin:"5px 0"
+              }
+            }}>
+              <form onSubmit={() => updatePostMutation.mutate(edit.id)}>
+                <TextField label="Title" name="edit_title" value={edit.edit_title} onChange={changeFormEdit} required /> <br />
+                <TextField  label="Body" name="edit_body" multiline maxRows={6} fullWidth value={edit.edit_body} onChange={changeFormEdit} required /> <br />
+                <Button 
+                  type="submit"
+                  color="info"
+                  variant="outlined"  
+                >Submit</Button>
+              </form>
+            </Box>
+          </div>
+        </div>
+      )}
 
-    <form onSubmit={handleCreate}>
-        Title: <input name="title" value={createInfo.title} onChange={(event: ChangeEvent<HTMLInputElement>): void => changeFormCreate(event)} required /> <br/>
-        Body: <textarea name="body" value={createInfo.body} onChange={(event: ChangeEvent<HTMLTextAreaElement>): void => changeFormCreate(event)} required/> <br/>
-        <button type="submit">Create Post</button>
-        </form>
-      <table border={1} cellPadding={0} cellSpacing={0}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>User ID</th>
-            <th>Title</th>
-            <th>Body</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data && data.map((item: TableDataProps, index) => {
-            if (item)
-              return (
-                <tr key={index}>
-                  <td>
-                    {item.id}
-                  </td>
-                  <td>
-                    {item.userId}
-                  </td>
-              
-                  <td>
-                    {item.title}
-                  </td>
-                  <td>
-                    {item.body}
-                  </td>
-                  <td>
-                    <button onClick = {() => handleUpdate(item)}>Update</button>
-                    <button onClick = {() => handleDelete(item.id)}> Delete </button>
-                  </td>
-                </tr>
+      <Box sx={{
+        textAlign: "left",
+        ".MuiTextField-root":{
+          margin:"10px 0"
+        },
+        ".MuiButton-root":{
+          margin:"10px 0"
+        }
+      }}>
+      <form
+          onSubmit={handleCreate}>
+        <TextField
+          name="title"
+          label="Title"
+          variant="outlined"
+          value={createInfo.title}
+          onChange={(event: ChangeEvent<HTMLInputElement>): void => changeFormCreate(event)}
+          required
+          size="small"
+        />
+        <br/>
+        <TextField 
+          name="body"
+          label="Body" 
+          multiline 
+          maxRows={4} 
+          variant="outlined" 
+          value={createInfo.body} 
+          onChange={(event: ChangeEvent<HTMLTextAreaElement>): void => changeFormCreate(event)} 
+          required 
+          size="small"
+        />
+        <br/>
+
+        <Button type="submit" variant="outlined" color="info">Create Post</Button>
+      </form>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell>ID</StyledTableCell>
+              <StyledTableCell align="center">User ID</StyledTableCell>
+              <StyledTableCell align="center">Title</StyledTableCell>
+              <StyledTableCell align="center">Body</StyledTableCell>
+              <StyledTableCell align="center">Action</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {data && data.map((item, index) => (
+              item && (
+                <StyledTableRow
+                key={index}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <StyledTableCell component="th" scope="row">
+                  {item.id}
+                </StyledTableCell>
+                <StyledTableCell align="center">{item.userId}</StyledTableCell>
+                <StyledTableCell align="center">{item.title}</StyledTableCell>
+                <StyledTableCell align="center">{item.body}</StyledTableCell>
+                <StyledTableCell align="center">
+                  <Button color="info" onClick={() => handleUpdate(item)}>Update</Button>
+                  <Button color="error" onClick={() => handleDelete(item.id)}> Delete </Button>
+                </StyledTableCell>
+              </StyledTableRow>
               )
-          })}
-        </tbody>
-      </table>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
       <div className="pagination">
         {paginate &&
           paginate.map((item, key) => {
